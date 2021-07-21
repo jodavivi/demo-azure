37e6b05875079ebdf751255deb79c854c8167891
David Villanueva <jodavivi@gmail.com>
2021-07-21 00:04:11 -0500

    Version 4

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
  
 var express = require('express'); 
-var http = require('http');
-var path = require('path');
+var http = require('http'); 
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000); 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
- 
- 
-http.createServer(app).listen(app.get('port'), function(req, res){
-    console.log('Express server listening on port ' + app.get('port'));
+
+const server = http.createServer(function (req, res) {
     res.writeHead(200, {'content-type': 'text/plain'});
     res.end('Hola Mundo');
 });
- 
+server.listen(app.get('port'));
